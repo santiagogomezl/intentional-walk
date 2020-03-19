fd21efd5adb7081c299b2c7beb9ec5d5ec88cdc6
Francis Li <mail@francisli.com>
2020-03-19 13:01:20 -0700

    Start/Stop/Save record tweaks

--- components/recorder.js
+++ components/recorder.js
@@ -1,7 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {useSafeArea} from 'react-native-safe-area-context';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Button from './button';
 import {Colors, GlobalStyles} from '../styles';
 import {Fitness} from '../lib';
 import moment from 'moment';
@@ -9,11 +10,19 @@ import numeral from 'numeral';
 
 export default function Recorder(props) {
   const safeAreaInsets = useSafeArea();
-  const [pedometerData, setPedometerData] = useState(null);
+  const [data, setData] = useState(null);
   const [now, setNow] = useState(new Date());
+  const [pause, setPause] = useState(null);
+  const isPausedRef = useRef(false);
+  const [end, setEnd] = useState(null);
+  const isEndedRef = useRef(false);
 
   useEffect(() => {
-    Fitness.startUpdates(pedometerData => setPedometerData(pedometerData));
+    Fitness.startUpdates(data => {
+      if (!isEndedRef.current && !isPausedRef.current) {
+        setData(data)
+      }
+    });
     return () => Fitness.stopUpdates();
   }, []);
 
@@ -22,13 +31,37 @@ export default function Recorder(props) {
     return () => clearInterval(interval);
   }, []);
 
-  let elapsedTime;
-  if (props.activeWalk) {
-    const dt = moment(now).diff(props.activeWalk.start, 'seconds');
-    const sec = dt % 60;
-    const min = Math.floor(dt / 60);
-    elapsedTime = `0${min}:${sec < 10 ? '0' : ''}${sec}`
+  const onPause = () => {
+
+  };
+
+  const onStop = () => {
+    let end = new Date();
+    setEnd(end);
+    isEndedRef.current = true;
+    Fitness.stopUpdates();
+    Fitness.getPedometerData(end).then(pedometerData => setData(data));
+  };
+
+  const onFinish = () => {
+    if (end) {
+      Fitness.stopRecording(end, data);
+    }
   }
+
+  let dt = 0, elapsedTime;
+  if (props.activeWalk) {
+    let compare = now;
+    if (end) {
+      compare = end;
+    } else if (pause) {
+      compare = pause;
+    }
+    dt = moment(compare).diff(props.activeWalk.start, 'seconds');
+  }
+  const sec = dt % 60;
+  const min = Math.floor(dt / 60);
+  elapsedTime = `${min < 10 ? '0' : ''}${min}:${sec < 10 ? '0' : ''}${sec}`
 
   return (
     <View pointerEvents="box-none" style={[styles.container, props.style]}>
@@ -41,30 +74,35 @@ export default function Recorder(props) {
           <Text style={styles.label}>min</Text>
         </View>
         <View>
-          <Text style={styles.count}>{pedometerData ? numeral(pedometerData.distance * 0.000621371).format('0.0') : '0.0'}</Text>
+          <Text style={styles.count}>{data ? numeral(data.distance * 0.000621371).format('0.0') : '0.0'}</Text>
           <Text style={styles.label}>miles</Text>
         </View>
         <View>
-          <Text style={styles.count}>{pedometerData ? pedometerData.numberOfSteps : 0}</Text>
+          <Text style={styles.count}>{data ? data.numberOfSteps : 0}</Text>
           <Text style={styles.label}>steps</Text>
         </View>
-      </View>
-      <View style={[styles.buttonsContainer, {paddingBottom: safeAreaInsets.bottom}]}>
-        <View style={styles.secondaryButtonContainer}>
-        </View>
-        <View style={styles.primaryButtonContainer}>
-          <TouchableOpacity onPress={() => Fitness.stopRecording() }>
-            <Image style={styles.primaryButton} source={require('../assets/stop.png')} />
-          </TouchableOpacity>
-          <Text style={[styles.buttonText, styles.recordText]}>Stop & Save</Text>
-        </View>
-        <View style={styles.secondaryButtonContainer}>
-          <TouchableOpacity style={styles.primaryButton}>
-            <Image style={styles.secondaryButton} source={require('../assets/pause.png')} />
-          </TouchableOpacity>
-          <Text style={[styles.buttonText, styles.pauseText]}>Pause</Text>
+        <View style={{opacity: end ? 1 : 0}}>
+          <Button onPress={onFinish} style={styles.finishButton}>Finish</Button>
         </View>
       </View>
+      { !end &&
+        <View style={[styles.buttonsContainer, {paddingBottom: safeAreaInsets.bottom}]}>
+          <View style={styles.secondaryButtonContainer}>
+          </View>
+          <View style={styles.primaryButtonContainer}>
+            <TouchableOpacity onPress={onStop}>
+              <Image style={styles.primaryButton} source={require('../assets/stop.png')} />
+            </TouchableOpacity>
+            <Text style={[styles.buttonText, styles.recordText]}>Stop & Save</Text>
+          </View>
+          <View style={styles.secondaryButtonContainer}>
+            <TouchableOpacity style={styles.primaryButton}>
+              <Image style={styles.secondaryButton} source={require('../assets/pause.png')} />
+            </TouchableOpacity>
+            <Text style={[styles.buttonText, styles.pauseText]}>Pause</Text>
+          </View>
+        </View>
+      }
     </View>
   );
 }
@@ -109,6 +147,9 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: Colors.primary.purple,
     marginBottom: 20
+  },
+  finishButton: {
+    width: 180
   },
   buttonsContainer: {
     position: 'absolute',
